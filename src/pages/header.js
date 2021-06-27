26905f47aef3d9812d736567b82b5fcad4f4843e
Aleksandra Dovgal <olexandra.dovgal@gmail.com>
2021-06-27 18:05:49 +0300

    add JS

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,6 +1,22 @@
-let tabsTitle = document.querySelectorAll('.menu__menu-tab');
-let tabsContent = document.querySelectorAll('.menu__menu-item');
-let tabs = document.querySelector('.menu__menu-tabs');
+const tabsTitle = document.querySelectorAll('.menu__menu-tab');
+const tabsContent = document.querySelectorAll('.menu__menu-item');
+const tabs = document.querySelector('.menu__menu-tabs');
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".burger-menu__button")
+const burgerMenu = document.querySelector(".burger-menu")
+const closeButton = document.querySelector(".square-btn")
+
+
+
+function toggleMenu(e){
+    e.preventDefault()
+    menu.classList.toggle("menu--active")
+    burgerMenu.classList.toggle("active")
+}
+
+menuButton.addEventListener('click', (e) => toggleMenu(e))
+closeButton.addEventListener('click', (e) => toggleMenu(e))
+
 tabs.addEventListener('click', function (event) {
     let target = event.target;
         for (let i = 0; i < tabsTitle.length; i++) {
@@ -12,3 +28,5 @@ tabs.addEventListener('click', function (event) {
                 tabsContent[i].classList.remove('tabs-par-show');
             }
 }});
+
+// menu.addEventListener("click", toggleMenu);
