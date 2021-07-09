1377e852ae7bc45cae49db403aca2f93efd96926
Aleksandra Dovgal <olexandra.dovgal@gmail.com>
2021-07-09 16:44:13 +0300

    add new structure

--- src/pages/header.js
+++ src/pages/header.js
@@ -1,6 +1,6 @@
-const tabsTitle = document.querySelectorAll('.menu__menu-tab-link');
-const tabsContent = document.querySelectorAll('.menu__menu-item');
-const tabs = document.querySelector('.menu__menu-tabs');
+const tabsTitle = document.querySelectorAll('.menu__tab-link');
+const tabsContent = document.querySelectorAll('.menu__item');
+const tabs = document.querySelector('.menu__tabs');
 const menu = document.querySelector(".menu");
 const menuButton = document.querySelector(".burger-menu__button")
 const burgerMenu = document.querySelector(".burger-menu")
@@ -21,30 +21,14 @@ tabs.addEventListener('click', function (event) {
     let target = event.target;
         for (let i = 0; i < tabsTitle.length; i++) {
            if (target === tabsTitle[i]) {
-               tabsTitle[i].classList.add('menu__menu-tab-link--active');
-
-               tabsContent[i].classList.add('menu__menu-item--active');
+               tabsTitle[i].classList.add('menu__tab-link--active');
+               tabsContent[i].classList.add('menu__item--active');
             } else {
-                tabsTitle[i].classList.remove('menu__menu-tab-link--active');
-                tabsContent[i].classList.remove('menu__menu-item--active');
+                tabsTitle[i].classList.remove('menu__tab-link--active');
+                tabsContent[i].classList.remove('menu__item--active');
             }
 }});
 
 // menu.addEventListener("click", toggleMenu);
 
 
-
-// const obj = {
-//     cards: ['gold', 'medium'],
-// }
-
-
-// document.querySelector('.menu-link').addEventListener('click', (e) => {
-//          e.target.textContent
-//         for( let [key, value] of Object.entries(obj)) {
-//             if(key === e.target.id) {
-//                 value forEach map
-//             }
-//         }
-//         })
-// })
